6a94aad76b148805505a50586bd59df831038f2f
Tara Kelly <kellyt2@students.westerntc.edu>
2020-08-09 12:41:28 -0500

    commented out some more annotations in Student class for ease of testing

--- ClassRegistrationProject/src/main/java/com/cognixia/jump/controller/StudentController.java
+++ ClassRegistrationProject/src/main/java/com/cognixia/jump/controller/StudentController.java
@@ -2,9 +2,14 @@ package com.cognixia.jump.controller;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +43,16 @@ public class StudentController {
 		}
 
 		return new Student();
+	}
+	
+	@PostMapping("/add/student")
+	public ResponseEntity<String> addStudent(@Valid @RequestBody Student newStudent) {
+		if(service.existsById(newStudent.getId())) {
+			return ResponseEntity.status(400).body("Account with id = " + newStudent.getId() + " already exists.");
+		} else {
+			Student created = service.save(newStudent);
+			return ResponseEntity.status(201).body("Created: " + created);
+		}
 	}
 
 }
